4dc5fab141dfc90c188cac072501cd05f62bafeb
akdombrowski <anthonydombrowski@pingidentity.com>
2018-11-13 14:16:25 -0600

    Multiplayer fixing name table and form.

--- src/App.js
+++ src/App.js
@@ -39,50 +39,51 @@ class App extends Component {
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleLifeChange = this.handleLifeChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleNameChange(event) {
-    let x = this.state.players.slice();
-    if (this.state.life != null && this.state.Life < 0) {
-      x.push({name: this.state.name, life: event.target.value})
-      this.setState({
-        players: x,
-        newPlayer: null,
-        newLife: null
-      })
-    } 
-    else{
-      this.setState({
-        players: x,
-        newPlayer: event.target.value,
-        newLife: null
-      });
-    }
+    let x = this.state.players;
+    let y = this.state.newPlayer;
+    this.setState({
+      players: x,
+      newPlayer: event.target.value,
+      newLife: y
+    });
   }
 
   handleLifeChange(event) {
     let x = this.state.players.slice();
-    if (this.state.newPlayer != null && this.state.newPlayer != "") {
-      x.push({name: this.state.name, life: event.target.value})
-      this.setState({
-        players: x,
-        newPlayer: null,
-        newLife: null
-      })
-    } 
-    else{
-      this.setState({
-        players: x,
-        newPlayer: null,
-        newLife: event.target.value
-      });
-    }
-    
+    let y = this.state.newPlayer;
+    console.log(event.target.value);
+    this.setState({
+      players: x,
+      newPlayer: y,
+      newLife: event.target.value
+    });
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.players.toString() + ' ' + this.state.newPlayer + ' ' + this.state.newLife);
+    console.log('submitted: ' + this.state.players[0] + ' ' + this.state.newPlayer + ' ' + this.state.newLife);
     event.preventDefault();
+    let arr = this.state.players.slice();
+    let n = this.state.newPlayer;
+    let l = this.state.newLife;
+    arr.push({name: n, life: l});
+    this.setState({
+      players: arr,
+      newPlayer: null,
+      newLife: null
+    });
+  }
+
+  handleReset(event) {
+    event.preventDefault();
+    this.setState({
+      players: [],
+      newPlayer: null,
+      newLife: null
+    });
   }
 
   render() { 
@@ -90,29 +91,33 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <table>
+          <table className="main-table">
+            <tr>
+              <th className="main-table-headers">Player</th>
+              <th className="main-table-headers">Life</th>    
+            </tr>
             <tbody>
-          {this.state.players.map((state) => {
-            return (
-              <tr>
-                <td key={state.name}>{state.name}: {state.life}</td>
-              </tr>
-            );
-          })}
-          </tbody>
+              {this.state.players.map((state) => {
+                return (
+                  <tr>
+                    <td key={state.name}>{state.name}:</td><td key="table-life-value"> {state.life}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
           </table>
-          <div className="Add-player">
-
+          <div className="add-player">
             <form id="addp" className="player-form" onSubmit={this.handleSubmit}>
               Player Name:
               <input type="text" name="Player Name" onChange={this.handleNameChange}></input>
               <br></br>
               Life Value:
-              <input type="number" life="Life Total" onChange={this.handleLifeChange}></input>
+              <input type="number" name="Life Total" min="0" placeholder="40" onChange={this.handleLifeChange}></input>
               <br></br>
               <input type="submit" value="Add Player">
               </input>
             </form>
+            <button className="reset-button" onClick={this.handleReset}>Reset</button>
           </div>
         </header>
       </div>
